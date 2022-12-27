1db65cb44b8cd8c1e388be6b43d393140a905eca
Fabian Unger <121499538+FabUnger@users.noreply.github.com>
2022-12-27 18:14:15 +0100

    Update UNO.hpp

--- UNO.hpp
+++ UNO.hpp
@@ -12,24 +12,41 @@ enum class Color
 
 enum class Action
 {
-	zweiZiehen,
-	vierZiehen,
-	aussetzen,
-	farbeWechseln
+	// Diese Aktionen werden als weitere Nummern in der Karte definiert
+	aussetzen,		// = 10
+	rotieren,		// = 11
+	zweiZiehen,		// = 12
+	vierZiehen,		// = 13
 };
 
 class Card
 {
 public:
+	int id;
 	int number;
 	Color color;
+	Card(int setID)
+	{
+		id = setID;
+	}
+
+	// Destruktor?
+};
+
+class Player
+{
+public:
+	int id;
+	std::vector<Card*> playerCards;
 };
 
 
 void menu();
 void changeRules();
 void showRules();
-void startGame();
+void startMultiplayer();
 int selectPlayerAmount();
+void showRanking();
+void createCards(std::vector<Card*> deck);
 
 #endif
