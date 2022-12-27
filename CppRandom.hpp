b6161d8bbccd62916dd0e9708aa4949fdf79e805
Fabian Unger <121499538+FabUnger@users.noreply.github.com>
2022-12-27 18:14:40 +0100

    Add files via upload

---
+++ CppRandom.hpp
@@ -0,0 +1,17 @@
+#ifndef CPP_RANDOM_HPP
+#define CPP_RANDOM_HPP
+
+#include <random>
+
+//Will be used to obtain a seed for the random number engine
+std::random_device rd;
+//Standard mersenne_twister_engine seeded with rd()
+std::mt19937 gen(rd());
+
+int GetRandomNumberBetween(int lower, int upper) {
+    std::uniform_int_distribution<> dis(lower, upper);
+    return dis(gen);
+}
+
+
+#endif
